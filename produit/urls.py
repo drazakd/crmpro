3cf6999e918d7bf99f56cde23f7147eab82b63da
drazakd <drazakd@gmail.com>
2024-04-01 15:54:18 +0000

    CRUD_produit_OK

--- produit/urls.py
+++ produit/urls.py
@@ -4,5 +4,11 @@ from . import views
 urlpatterns = [
     path('produit', views.produit, name='produit'),
     path('ajouter_categorie', views.ajouter_categorie, name='ajouter_categorie'),
-    path('ajouter_produit', views.ajouter_produit, name='ajouter_produit')
+    path('modifier_categorie/<str:pk>', views.modifier_categorie, name='modifier_categorie'),
+    path('supprimer_categorie/<str:pk>', views.supprimer_categorie, name='supprimer_categorie'),
+
+    path('ajouter_produit', views.ajouter_produit, name='ajouter_produit'),
+    path('modifier_produit/<str:pk>', views.modifier_produit, name='modifier_produit'),
+    path('supprimer_produit/<str:pk>', views.supprimer_produit, name='supprimer_produit')
 ]
+
--- produit/views.py
+++ produit/views.py
@@ -9,16 +9,42 @@ def produit(request):
     context = {'produits': produits, 'categories': categories}
     return render(request, 'produit/produit.html', context)
 
+
+
 def ajouter_categorie(request):
     form = CategorieForm()
     if request.method == 'POST':
         form = CategorieForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('')
+            return redirect('produit')
 
     context = {'form': form}
     return render(request, 'produit/ajouter_categorie.html', context)
+
+def modifier_categorie(request,pk):
+    categorie = Categorie.objects.get(id_categorie=pk)
+    form = CategorieForm()
+
+    if request.method == 'POST':
+        form = CategorieForm(request.POST, instance=categorie)
+        if form.is_valid():
+            form.save()
+            return redirect('produit')
+
+    context = {'form':form}
+    return render(request, 'produit/ajouter_categorie.html', context)
+
+def supprimer_categorie(request,pk):
+    categorie = Categorie.objects.get(id_categorie=pk)
+    if request.method == 'POST':
+        categorie.delete()
+        return redirect('produit')
+
+    context = {'item': categorie}
+    return render(request, 'produit/supprimer_categorie.html', context)
+
+
 
 def ajouter_produit(request):
     form = ProduitForm()
@@ -26,7 +52,29 @@ def ajouter_produit(request):
         form = ProduitForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('')
+            return redirect('produit')
 
     context = {'form': form}
     return render(request, 'produit/ajouter_produit.html',context)
+
+def modifier_produit(request,pk):
+    produit = Produit.objects.get(id_produit=pk)
+    form = ProduitForm(instance=produit)
+
+    if request.method == 'POST':
+        form = ProduitForm(request.POST, instance=produit)
+        if form.is_valid():
+            form.save()
+            return redirect('produit')
+
+    context = {'form': form}
+    return render(request, 'produit/ajouter_produit.html', context)
+
+def supprimer_produit(request,pk):
+    produit = Produit.objects.get(id_produit=pk)
+    if request.method == 'POST':
+        produit.delete()
+        return redirect('produit')
+
+    context = {'item': produit}
+    return render(request, 'produit/supprimer_produit.html', context)
