ff6f4184820b1f1163c52b5da5330858ca124550
drazakd <drazakd@gmail.com>
2024-05-06 15:37:37 +0000

    ajout du cahier de specificité

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path('',views.home,name='home'),
     path('signup',views.signup,name='signup'),
-    path('adminreg',views.adminreg,name='adminreg'),
+    path('superadmin',views.adminreg,name='superadmin'),
     path('log_out',views.logout_view,name='log_out')
 ]
